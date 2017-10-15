786676b659c7a774f1909a5ecf4e11228862cba9
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-15 01:08:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-  array.join(" and ")
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif arary.length > 2
+    array[-1] = "and #{array[-1]}"
+  else
+    nil
+  end
 end
